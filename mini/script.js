ddee53dabce7ba8eb07b50be589ee3d83db0218c
ramixca <70819559+ramixca@users.noreply.github.com>
2024-03-06 21:11:41 -0800

    Update script.js
    Created functions for newQuote() tweetQuote() and eventlisteners for buttons

--- mini/script.js
+++ mini/script.js
@@ -1,12 +1,30 @@
 // Generate Quotes from API 
 
+const quoteContainer = document.getElementById('main-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('x-twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // FUNCTION TO SHOW NEW QUOTE
 function newQuote() {
     // math.floor to round down to nearest whole number
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote)
+    // Check if Author field is blank and replace it with 'Unknown' copilot
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+// TO REDUCE FONT SIZE FOR LONG QUOTES
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
 }
 
 async function getQuotes() {
@@ -20,5 +38,18 @@ async function getQuotes() {
         // Catch Error Here
     }
 }
+
+
+
+// X-TWITTER FUNCTION quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 getQuotes();
